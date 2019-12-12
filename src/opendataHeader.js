5289208de57cda688d7bcf7421c46e4976c64704
Gase18 <42963888+Gase18@users.noreply.github.com>
2019-12-12 10:01:53 +0100

    Header with login and bulma implemented

---
+++ src/opendataHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+export function OpendataHeader(props){
+    return <div className="Header">
+        <label className="uname" htmlFor="username"><b>Username</b></label>
+            <input type="text" id="username"></input>
+        <label className="psw" htmlFor="password"><b>Password</b></label>
+            <input type="password" id="password"></input>
+            <button>Login</button>
+            </div>
+}
