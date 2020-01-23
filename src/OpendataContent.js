d0806bf7d0ec7b55d8076f707abfcb1a37b1132b
Gase18 <42963888+Gase18@users.noreply.github.com>
2020-01-23 11:49:37 +0100

    Grid with pictures

---
+++ src/OpendataContent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './index.scss';
+
+
+export function OpendataContent(props){
+    
+    return <div className ="FoodContainer">
+        <div className = "FoodContent">
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+            <li><a href="pics\art.jpg"><img src="pics\art.jpg"/></a></li>
+
+            
+        </div>
+
+    </div>
+
+}
