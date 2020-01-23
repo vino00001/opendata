af13da416f5bfed39493161231d3ac7be460d6cb
vino00001 <54398414+vino00001@users.noreply.github.com>
2020-01-23 09:47:07 +0100

    Small cleanup

--- src/opendataHeader.js
+++ src/opendataHeader.js
@@ -3,10 +3,10 @@ import './index.scss';
 
 export function OpendataHeader(props){
     return <div className="Header">
-        <label className="uname" htmlFor="username"><b>Username</b></label>
-            <input type="text" id="username"></input>
-        <label className="psw" htmlFor="password"><b>Password</b></label>
-            <input type="password" id="password"></input>
-            <button>Login</button>
+                <label className="uname" htmlFor="username"><b>Username</b></label>
+                <input type="text" id="username"></input>
+                <label className="psw" htmlFor="password"><b>Password</b></label>
+                <input type="password" id="password"></input>
+                <button>Login</button>
             </div>
 }
